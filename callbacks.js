11447773b7fcd6669f8bf99994c3b438a8a13362
ankur0904 <ankursingh91002@gmail.com>
2025-08-09 10:08:18 +0530

    lecture one

--- callbacks.js
+++ callbacks.js
@@ -1,9 +1,56 @@
 console.log('JS');
 
 
-setTimeout(function (){
+setTimeout(function () {
     console.log('JS2');
 }, 5000)
 
 
 console.log('JS3');
+
+// E commerce example
+
+const cart = ["white shoes", "poloride glasses", "analogue watch", "Samsung mobile"];
+
+api.CreateOrder();
+api.ProceedToPayment();
+
+// We nee to only proceedPayment after the creating the order
+
+api.CreateOrder(cart, function () {             // <-- responsibility of CreateOrder API to create an order and then call the fucntion back 
+    api.ProceedToPayment();
+})
+
+// Making things interesting
+// show the order summary as well
+
+api.CreateOrder(cart, function () {
+
+    api.ProceedToPayment(function () {          // <-- responsibility of ProceedToPayment API to do payment and then call the fucntion back 
+
+        api.showOrderSummary()
+
+    });
+})
+
+
+
+// callback hell (or) pyramid of doom: code is growing horizontally not vertically
+api.CreateOrder(cart, function () {
+
+    api.ProceedToPayment(function () {
+
+        api.showOrderSummary(function() {
+
+            api.updateWalletBalance();
+
+        })
+        
+    });
+})
+
+// Inversion of control
+api.CreateOrder(cart, function () {    // <- we give the control of our code to "createOrder" API, now it's responsibility of "createOrder" API to call our functin back, we are blindly trusting the API
+
+    api.ProceedToPayment();
+})
