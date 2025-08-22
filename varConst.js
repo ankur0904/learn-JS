ed19ccf9757616208ffabcb3fb186b5fa40be218
Ankur Singh <ankursingh91002@gmail.com>
2025-08-22 19:34:48 +0530

    Add files via upload

---
+++ varConst.js
@@ -0,0 +1,21 @@
+// console.log(a);
+// console.log(b);
+// let a = 10;
+// console.log(a);
+// var b = 100;
+// console.log(b);
+
+let a = 10;
+
+// let a = 20; *not allowed*
+
+var b = 20;
+var b = 300; // allowed
+
+// const same as let but don't allow re-assignment
+
+// TypeError: Type is not same
+// Reference Error: Access the variable which has not taken memory till
+// Syntax Error: syntax not correct
+
+// uses => const, let, var 
